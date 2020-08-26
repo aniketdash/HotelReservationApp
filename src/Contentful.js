99aef654fcd1db6aebf962240cfd01022e29fd26
Aniket Dash <dash.aniket05@gmail.com>
2020-08-25 20:44:18 -0400

    deploy fix

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space: "bz7ttl2acl1o",
-    accessToken: "RqPWDg3rHYoCe6o5Fq1Aqzr6p2h6hVVBAwbs"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
